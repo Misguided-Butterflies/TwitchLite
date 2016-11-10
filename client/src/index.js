a945f1b107f11172cd89438e6a671bc16c20ebf7
sam sherman <samdsherman@gmail.com>
2016-11-09 19:57:47 -0800

    create header component

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {render} from 'react-dom';
+import Header from './Header';
 
 
 class App extends React.Component {
   render() {
     return (
-      <div> Hello World </div>
+      <div>
+        <Header />
+      </div>
     );
   }
 }
