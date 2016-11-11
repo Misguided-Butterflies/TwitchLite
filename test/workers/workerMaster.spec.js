d309e9564b4705ab492a93f2420b800a3b563a05
John Oksasoglu <john.oksasoglu@gmail.com>
2016-11-10 18:56:39 -0800

    Add workerMaster tests

--- test/workers/workerMaster.spec.js
+++ test/workers/workerMaster.spec.js
@@ -2,6 +2,8 @@ var Promise = require('node-fetch').Promise;
 var workerMaster = require('../../workers/workerMaster');
 // var worker = require('../../workers/worker');
 
+var sampleChannel = 'GosuTV_OW';
+
 describe('workerMaster', function() {
   beforeEach(function() {
     var activeWorkerNames = Object.keys(workerMaster.getWorkers());
@@ -11,17 +13,15 @@ describe('workerMaster', function() {
   });
 
   it('should be able to add workers', function() {
-    var channelName = 'twitch';
     expect(workerMaster.getWorkers()).to.eql({});
-    workerMaster.addWorker(channelName);
-    expect(workerMaster.getWorkers()[channelName]).to.exist;
+    workerMaster.addWorker(sampleChannel);
+    expect(workerMaster.getWorkers()[sampleChannel]).to.exist;
   });
 
   it('should be able to remove workers', function() {
-    var channelName = 'twitch';
-    workerMaster.addWorker(channelName);
-    workerMaster.removeWorker(channelName);
-    expect(workerMaster.getWorkers()[channelName]).to.not.exist;
+    workerMaster.addWorker(sampleChannel);
+    workerMaster.removeWorker(sampleChannel);
+    expect(workerMaster.getWorkers()[sampleChannel]).to.not.exist;
   });
 
   describe('getTopStreams', function() {
@@ -33,9 +33,6 @@ describe('workerMaster', function() {
 
     it('should fetch the top Twitch streams', function(done) {
       workerMaster.getTopStreams()
-      .then(response => {
-        return response.json();
-      })
       .then(data => {
         // 25 is an arbitrary number, but we want to ensure we always have a
         // sizable portion at least
@@ -43,8 +40,28 @@ describe('workerMaster', function() {
         done();
       })
       .catch(error => {
-        console.error('Failed to fetch top 50 streams:', error);
+        console.error('Failed to fetch top streams:', error);
       });
     });
   });
+
+  describe('getStreamVODID', function() {
+    it('should return a Promise', function() {
+      var result = workerMaster.getStreamVODID(sampleChannel);
+
+      expect(result).to.be.an.instanceOf(Promise);
+    });
+
+    it('should fetch the VOD ID of the channel\'s latest broadcast', function(done) {
+      workerMaster.getStreamVODID(sampleChannel)
+      .then(id => {
+        expect(typeof id).to.equal('string');
+        done();
+      })
+      .catch(error => {
+        console.error('Error encountered:', error);
+      });
+    });
+  });
+
 });
