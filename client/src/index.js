0468bb8bf1be343431013c83a6d3ae9b17c4ee50
sam sherman <samdsherman@gmail.com>
2016-11-11 16:01:10 -0800

    Create VideoList and add a test for it

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Header from './Header';
-import Video from './Video';
+import VideoList from './VideoList';
 
+let testVideos = [
+  {
+    id: 'v97978712',
+    start: 7206,
+    duration: 6,
+  }, {
+    id: 'v99478815',
+    start: 9342,
+    duration: 55,
+  }, {
+    id: 'v100239687',
+    start: 22,
+    duration: 9,
+  }
+];
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header />
-        <Video video={{
-          start: 3600,
-          id: 'v97978712',
-          duration: 10,
-        }} />
-        <Video video={{
-          start: 7206,
-          id: 'v97978712',
-          duration: 6,
-        }} />
+        <VideoList list={testVideos} />
       </div>
     );
   }
