3c123796b1a85811e6676612c501b23d25ce82dd
John Oksasoglu <john.oksasoglu@gmail.com>
2016-11-16 12:09:12 -0800

    Clean up <VideoList>

--- client/src/VideoList/index.js
+++ client/src/VideoList/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import VideoContainer from '../VideoContainer';
 
-const numberOfSecondsToAddToBeginningOfHighlights = 30;
-
 /** VideoList
  * a collection of VideoContainer components.
  * usage: <VideoList list={[video1, video2, video3]} />
@@ -12,14 +10,7 @@ const VideoList = function(props) {
   return (
     <div className='video-list'>
       {props.list.map((video, i) => (
-        <div key={i}>
-          <VideoContainer video={{
-            id: video.vodId,
-            preview: video.preview,
-            start: Math.floor((video.highlightStart - video.streamStart) / 1000) - numberOfSecondsToAddToBeginningOfHighlights,
-            duration: Math.floor((video.highlightEnd - video.highlightStart) / 1000) + numberOfSecondsToAddToBeginningOfHighlights
-          }} />
-        </div>
+        <VideoContainer key={i} video={video} />
       ))}
     </div>
   );
