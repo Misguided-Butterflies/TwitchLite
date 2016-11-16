bbf4a029936c1b3167349d05ebf3b29b0e15694f
John Oksasoglu <john.oksasoglu@gmail.com>
2016-11-16 10:45:57 -0800

    Fix linting errors in client test

--- test/client/index.spec.js
+++ test/client/index.spec.js
@@ -1,6 +1,6 @@
 import Video from '../../client/src/Video';
-import VideoList from '../../client/src/VideoList'; 
- 
+import VideoList from '../../client/src/VideoList';
+
 let testVideos = [
   {
     id: 'v97978712',
@@ -19,26 +19,26 @@ let testVideos = [
 
 let testHighlights = [
   {
-    "highlightStart" : 1479151790954,
-    "highlightEnd" : 1479151793955,
-    "multiplier" : 5,
-    "link" : "https://www.twitch.tv/comanchedota/v/101060439",
-    "game" : "Dota 2",
-    "streamStart" : 1479145561000,
+    'highlightStart': 1479151790954,
+    'highlightEnd': 1479151793955,
+    'multiplier': 5,
+    'link': 'https://www.twitch.tv/comanchedota/v/101060439',
+    'game': 'Dota 2',
+    'streamStart': 1479145561000,
   }, {
-    "highlightStart" : 1479151790914,
-    "highlightEnd" : 1479151793916,
-    "multiplier" : 3.8888888888888884,
-    "link" : "https://www.twitch.tv/cartmanzbs/v/101042097",
-    "game" : "Dota 2",
-    "streamStart" : 1479137664000,
+    'highlightStart': 1479151790914,
+    'highlightEnd': 1479151793916,
+    'multiplier': 3.8888888888888884,
+    'link': 'https://www.twitch.tv/cartmanzbs/v/101042097',
+    'game': 'Dota 2',
+    'streamStart': 1479137664000,
   }, {
-    "highlightStart" : 1479151778948,
-    "highlightEnd" : 1479151796955,
-    "multiplier" : 4.6875,
-    "link" : "https://www.twitch.tv/kubon_/v/101042878",
-    "game" : "Gothic II: Night of the Raven",
-    "streamStart" : 1479138061000,
+    'highlightStart': 1479151778948,
+    'highlightEnd': 1479151796955,
+    'multiplier': 4.6875,
+    'link': 'https://www.twitch.tv/kubon_/v/101042878',
+    'game': 'Gothic II: Night of the Raven',
+    'streamStart': 1479138061000,
   }
 ];
 
