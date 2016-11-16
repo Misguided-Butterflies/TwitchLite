ecd6b19faa9f2998dfc6b46126beb898461a7204
John Oksasoglu <john.oksasoglu@gmail.com>
2016-11-16 12:34:47 -0800

    Add notes to <VideoContainer>

--- client/src/VideoContainer/index.js
+++ client/src/VideoContainer/index.js
@@ -1,32 +1,73 @@
 import React from 'react';
 import Video from '../Video';
+import axios from 'axios';
+
+const numberOfSecondsToAddToBeginningOfHighlights = 30;
 
 /** VideoComponent
  * this is a component that wraps <Video> and handles all other UI and logic
  * for rendering a <Video>
  * usage:
- * let video = {
- *   id: 'v97978712', // twitch video id
- *   start: 7206, // start time of the hightlight in seconds
- *   duration: 15, // duration of the highlight in seconds
- * };
- * // ...
  * <VideoContainer video={video} />
  */
-const VideoContainer = function(props) {
-  return (
-    <div className='video-container'>
-      <Video video={props.video} />
-    </div>
-  );
-};
+class VideoContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.sendVote = this.sendVote.bind(this);
+  }
+
+  calculateVotes() {
+    // this should be a pure function
+    // add up all the votes from all the users
+  }
+
+  sendVote() {
+    // Use Axios
+    // post vote data
+    // do we just assume that the video data otherwise is unchanged?
+    // if yes, then we're fine keeping video as a prop
+    // if no, then the prop needs to be used as initial state, which is updated
+    // based on the upvote data
+    // but the former option seems better
+    axios.post('/votes', {
+      // username
+      // video id
+      // vote
+    })
+    .then(updatedVideo => {
+
+    });
+  }
+
+  render() {
+    return (
+      <div className='video-container'>
+      <h2>{this.props.video.streamTitle}</h2>
+      <h3>Playing: {this.props.video.game}</h3>
+      <Video video={{
+        id: this.props.video.vodId,
+        preview: this.props.video.preview,
+        start: Math.floor((this.props.video.highlightStart - this.props.video.streamStart) / 1000) - numberOfSecondsToAddToBeginningOfHighlights,
+        duration: Math.floor((this.props.video.highlightEnd - this.props.video.highlightStart) / 1000) + numberOfSecondsToAddToBeginningOfHighlights
+      }} />
+      </div>
+    );
+  }
+}
 
 VideoContainer.propTypes = {
   video: React.PropTypes.shape({
-    id: React.PropTypes.string.isRequired,
-    start: React.PropTypes.number.isRequired,
-    duration: React.PropTypes.number.isRequired,
+    vodId: React.PropTypes.string.isRequired,
+    channelName: React.PropTypes.string.isRequired,
+    game: React.PropTypes.string.isRequired,
     preview: React.PropTypes.string.isRequired,
+    streamStart: React.PropTypes.number.isRequired,
+    streamTitle: React.PropTypes.string.isRequired,
+    highlightStart: React.PropTypes.number.isRequired,
+    highlightEnd: React.PropTypes.number.isRequired,
+    multiplier: React.PropTypes.number.isRequired,
+    votes: React.PropTypes.object.isRequired,
   }).isRequired,
 };
 
