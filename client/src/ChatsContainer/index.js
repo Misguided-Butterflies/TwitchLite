1f40efe8f57d146c872952dd4110c425df190de8
John Oksasoglu <john.oksasoglu@gmail.com>
2016-11-18 11:45:04 -0800

    Add label and keys in <ChatsContainer>

--- client/src/ChatsContainer/index.js
+++ client/src/ChatsContainer/index.js
@@ -4,9 +4,10 @@ import Chat from '../Chat';
 const ChatsContainer = ({messages}) => {
   return (
     <div className='chats-container'>
+      <h3>Chat</h3>
       {
         messages.map(message => (
-          <Chat message={message} />
+          <Chat key={message.from + message.time} message={message} />
         ))
       }
     </div>
