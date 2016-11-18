ab90302ec04cc1643355216bcb7c6cb0fc36e377
John Oksasoglu <john.oksasoglu@gmail.com>
2016-11-17 17:16:55 -0800

    Fill out <ChatsContainer>

--- client/src/ChatsContainer/index.js
+++ client/src/ChatsContainer/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Chat from '../Chat';
 
-class ChatsContainer extends React.Component {
-
-}
+const ChatsContainer = ({messages}) => {
+  return (
+    <div>
+      {
+        messages.map(message => (
+          <Chat message={message} />
+        ))
+      }
+    </div>
+  );
+};
 
 ChatsContainer.propTypes = {
-
+  messages: React.PropTypes.array.isRequired
 };
 
 export default ChatsContainer;
