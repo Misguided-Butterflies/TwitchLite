15162d244ce95f7063a8bd570802a441c883f8d3
John Oksasoglu <john.oksasoglu@gmail.com>
2016-11-19 14:42:07 -0800

    Refine styles

--- client/src/ChatsContainer/index.js
+++ client/src/ChatsContainer/index.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import Chat from '../Chat';
 
-const ChatsContainer = (props) => {
-  return (
-    <div className='chats-container'>
+class ChatsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.scrollDown = this.scrollDown.bind(this);
+  }
+
+  scrollDown() {
+    this.refs.container.scrollTop = this.refs.container.scrollHeight;
+  }
+
+  componentDidUpdate() {
+    this.scrollDown();
+  }
+
+  render() {
+    return (
+      <div className='chats-container' ref='container'>
       <h4 className='chat-header'>Chat Replay</h4>
-        {
-          props.messages.map(message => (
-            <Chat key={message.from + message.time} message={message} emotes={props.emotes} />
-          ))
-        }
-    </div>
-  );
-};
+      {
+        this.props.messages.map(message => (
+          <Chat key={message.from + message.time} message={message} emotes={this.props.emotes} />
+        ))
+      }
+      </div>
+    );
+  }
+
+}
 
 ChatsContainer.propTypes = {
   emotes: React.PropTypes.object.isRequired,
